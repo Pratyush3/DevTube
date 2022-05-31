8db8e4e19ee049bc6c2f31f409dae7e3531ab191
Pratyush Singh <pratyushsingh0309@gmail.com>
2022-05-31 20:37:35 +0530

    Made the navbar responsive

--- JS/navbar.js
+++ JS/navbar.js
@@ -3,3 +3,9 @@ document.getElementById("brand-logo").addEventListener('click', () => {
     location.reload();
 })
 //On clicking then responsive menu button
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navelems')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
